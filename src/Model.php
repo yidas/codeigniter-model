020f8c3810f4e369b3e054a77e03a01ed2d528ba
Nick Tsai <myintaer@gmail.com>
2018-05-14 13:42:12 +0800

    Release version 2.6.0

--- src/Model.php
+++ src/Model.php
@@ -6,7 +6,7 @@ namespace yidas;
  * Base Model
  *
  * @author   Nick Tsai <myintaer@gmail.com>
- * @version  2.5.0
+ * @version  2.6.0
  * @see      https://github.com/yidas/codeigniter-model
  */
 class Model extends \CI_Model
