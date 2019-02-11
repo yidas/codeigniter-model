92e6e371d2f07c28a787a778ebcf3b970e3d1792
Nick Tsai <myintaer@gmail.com>
2019-02-11 15:58:01 +0800

    Release version 2.12.1

--- src/Model.php
+++ src/Model.php
@@ -8,7 +8,7 @@ use Exception;
  * Base Model
  *
  * @author   Nick Tsai <myintaer@gmail.com>
- * @version  2.12.0
+ * @version  2.12.1
  * @see      https://github.com/yidas/codeigniter-model
  */
 class Model extends \CI_Model implements \ArrayAccess
